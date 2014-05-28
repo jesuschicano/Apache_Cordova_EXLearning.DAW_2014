f47ff95203c5b8dab600e1b4b7454d220ad889cc
jesuschicano <jesuschicano3988@gmail.com>
2014-05-28 14:46:29 +0200

    upload device-motion dialogs_plugin

---
+++ Dialogs_Plugin/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,32 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.dialogs": "0.2.7",
+    "org.apache.cordova.vibration": "0.3.8"
+}
+// BOTTOM OF METADATA
+});
